59a9a94a23621ff1044df590ec6c99b43805dad3
mdsafar <mdsafar2005@gmail.com>
2025-03-01 19:22:11 +0530

    token related updates

--- server/utils/jwtToken.js
+++ server/utils/jwtToken.js
@@ -3,10 +3,11 @@ export const sendToken = (user,statusCode,res)=>{
     const token = user.getJwtToken()
 
     const options = {
-        expires:new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
-        httpOnly:true,
-        secure:true
-    }
+        expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+        httpOnly: true, 
+        secure: process.env.NODE_ENV === "production", 
+        sameSite: "None",
+    };
 
     res.status(statusCode).cookie('token',token,options).json({
         success:true,
